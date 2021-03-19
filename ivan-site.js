83182250b9b86bd4a26edd32c05086d20ba6461c
Ivan Castillo <total180training@gmail.com>
2021-03-19 08:00:03 -0400

    Added interaction, both to nav menu and to single page contents. Included svg sprite for landing page

---
+++ ivan-site.js
@@ -0,0 +1,52 @@
+
+let aboutMeSection = document.getElementById("about-me")
+let projectsSection = document.getElementById("projects")
+let skillsSection = document.getElementById("skills")
+
+let aboutMeLink = document.getElementById("nav-about")
+let projectsLink = document.getElementById("nav-projects")
+let skillsLink = document.getElementById("nav-skills")
+
+let navPrompt = document.getElementById("nav-prompt")
+
+const myDefault = () => {
+    aboutMeSection.style.display = 'none'
+    projectsSection.style.display = 'none'
+    skillsSection.style.display = 'none'
+}
+
+
+
+aboutMeLink.addEventListener("click", () => {
+    myDefault()
+    aboutMeSection.style.display = "grid";
+    navPrompt.textContent = ''
+    navPrompt.textContent = 'We have arrived homonid. Enjoy your stay.'
+    setTimeout(function() {
+        navPrompt.style.paddingLeft = '30px';
+        navPrompt.innerHTML = 'You\'re still here homosapien?<br>Do you have a new location in mind?'
+    }, 5000)
+})
+
+projectsLink.addEventListener("click", () => {
+    myDefault()
+    projectsSection.style.display = "block"
+    navPrompt.textContent = ''
+    navPrompt.textContent = 'We have arrived homonid. Enjoy your stay.'
+    setTimeout(function() {
+        navPrompt.style.paddingLeft = '30px';
+        navPrompt.innerHTML = 'You\'re still here homosapien?<br>Do you have a new location in mind?'
+    }, 5000)
+})
+
+skillsLink.addEventListener("click", () => {
+    myDefault()
+    skillsSection.style.display = "grid"
+    navPrompt.textContent = ''
+    navPrompt.textContent = 'We have arrived homonid. Enjoy your stay.'
+    setTimeout(function() {
+        navPrompt.style.paddingLeft = '30px';
+        navPrompt.innerHTML = 'You\'re still here homosapien?<br>Do you have a new location in mind?'
+    }, 5000)
+})
+
