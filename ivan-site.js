b37b466bf0e38aed7594fda54e4ee8f753f7455f
IvanCastillo1986 <total180training@gmail.com>
2024-02-15 13:49:31 -0500

    added Cyber page. Adjusted styling

--- ivan-site.js
+++ ivan-site.js
@@ -1,5 +1,22 @@
 console.log('ivan-site.js works!')
 
+/* 
+index.html
+The Link is in the navigation bar
+The Section is in the Main page
+
+ivan-site.js
+NOTE: this is the flow for displaying a certain Section (looks like different page to user)
+
+get reference to cyberSection and create element variable
+get reference to cyberLink and create element variable
+add eventListener to click on cyberLink
+    run hideAllSections() to set all other elements to display = 'none'
+    display cyberSection by changing element css to display = 'block'
+    saySomethingElse()
+
+*/
+
 let main = document.querySelector('main')
 
 let goHome = document.getElementById('go-home')
@@ -7,20 +24,25 @@ let goHome = document.getElementById('go-home')
 let aboutMeSection = document.getElementById("about-me")
 let projectsSection = document.getElementById("projects")
 let skillsSection = document.getElementById("skills")
+let cyberSection = document.getElementById("cyber")
 
 let aboutMeLink = document.getElementById("nav-about")
 let projectsLink = document.getElementById("nav-projects")
 let skillsLink = document.getElementById("nav-skills")
+let cyberLink = document.getElementById("nav-cyber")
 
 let navPrompt = document.getElementById("nav-prompt")
 
-const myDefault = () => {
+
+// hide every <Section> on nav link click, so that we can display new section
+const hideAllSections = () => {
     aboutMeSection.style.display = 'none'
     projectsSection.style.display = 'none'
     skillsSection.style.display = 'none'
-    // main.style.display = 'none'
+    cyberSection.style.display = 'none'
 }
 
+// display Robot's default text. Will call whenever a nav link is clicked
 const saySomethingElse = () => {
     navPrompt.textContent = ''
     navPrompt.textContent = 'We have arrived homonid. Enjoy your stay. Scroll down.'
@@ -38,7 +60,7 @@ const saySomethingElse = () => {
 
 // function clickMe(link) {
 //     link.addEventListener("click", () => {
-//         myDefault()
+//         hideAllSections()
 //         link.style.display = "block";
 //         saySomethingElse()
 //     })
@@ -47,27 +69,33 @@ const saySomethingElse = () => {
 
 
 aboutMeLink.addEventListener("click", () => {
-    myDefault()
+    hideAllSections()
     aboutMeSection.style.display = "block";
     saySomethingElse()
 })
 
 projectsLink.addEventListener("click", () => {
-    myDefault()
+    hideAllSections()
     projectsSection.style.display = "block"
     saySomethingElse()
 })
 
 skillsLink.addEventListener("click", () => {
-    myDefault()
+    hideAllSections()
     skillsSection.style.display = "block"
+    saySomethingElse()
+})
+
+cyberLink.addEventListener("click", () => {
+    hideAllSections()
+    cyberSection.style.display = "block"
     saySomethingElse()
 })
 
 
 goHome.addEventListener('click', () => {
     navPrompt.innerHTML = 'Welcome traveler.<br>I am \'The Gatekeeper\'.<br>A sentient lifeform built by Ivan.<br><br>How would you like to proceed human?'
-    myDefault()
+    hideAllSections()
 
 })
 goHome.addEventListener('mouseover', () => {
